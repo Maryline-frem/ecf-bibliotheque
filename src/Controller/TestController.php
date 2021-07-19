f750cc923ffeb1e0a634062ee4b3ba2dde6abdc3
Maryline Frémineur <maryline.fremineur@gmail.com>
2021-07-19 10:05:06 +0200

    Ajout de requete et de fonctions  pour les livres

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Entity\Kind;
 use App\Entity\User;
 use App\Repository\BookRepository;
 use App\Repository\BorrowerRepository;
+use App\Repository\KindRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,6 +23,7 @@ class TestController extends AbstractController
      */
     public function index(
         BookRepository $bookRepository,
+        KindRepository $kindRepository,
         UserRepository $userRepository): Response
     {
         // Récupération de l'entity manager.
@@ -27,26 +31,49 @@ class TestController extends AbstractController
 
         // Récupération de tous les utilisateurs.
         $users = $userRepository->findAll();
-        dump($users);
+        // dump($users);
 
         // Récupération de l'utilisateur dont l'id est 1.
         $user = $userRepository->find(1);
-        dump($user);
+        // dump($user);
 
         // Récupération de l'utilisateur pas l'email.
         $mail = 'foo.foo@example.com';
-
         $user = $userRepository->findByEmail($mail);
-        dump($user);
+        // dump($user);
         
         // Récupération des utilisateurs dont l'attribut `roles` contient le mot clé `ROLE_EMRUNTEUR`.
-        // Requête a revoir !!!!!!!
-        // $users = $userRepository->findByRole('ROLE_EMRUNTEUR');
+        $users = $userRepository->findByRole('ROLE_EMPRUNTEUR');
         // dump($users);
 
         // Récupération de la liste complète de tous les livres.
         $books = $bookRepository->findAll();
-        dump($books);
+        // dump($books);
+
+        // Récupération des données du livre dont l'id est 1.
+        $book = $bookRepository->find(1);
+        // dump($book);
+
+        // Récupération de la liste des livres dont le titre contient le mot clé `lorem`.
+        $title = 'Lorem';
+        $book = $bookRepository->findByTitle($title);
+        // dump($book);
+
+        // Récupération de la liste des livres dont l'id de l'auteur est `2`.
+        $book = $bookRepository->findByAuthor(2);
+        dump($book);
+
+        // Récupération de la liste des livres dont le genre contient le mot clé `roman`.
+        // A revoir
+        // $kind = 'Roman';
+        // $book = $bookRepository->findByTitle($kind);
+        // dump($book);
+
+        // Ajouter un nouveau livre.
+
+        // Requêtes de mise à jour.
+
+        // Supprimer le livre dont l'id est `123`.
         exit();
     }
 }
--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -2,7 +2,9 @@
 
 namespace App\Repository;
 
+use App\Entity\Author;
 use App\Entity\Book;
+use App\Entity\Kind;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -17,6 +19,44 @@ class BookRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Book::class);
+    }
+
+    /**
+    * @return Book[] Returns an array of Book objects
+    */
+    public function findByTitle($value)
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.title LIKE :value')
+            ->setParameter('value', "%{$value}%")
+            ->orderBy('t.title', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findByAuthor(int $id)
+    {
+        return $this->createQueryBuilder('i')
+            ->innerJoin('i.author', 'a')
+            ->andWhere('a.id = :author')
+            ->setParameter('author', $id)
+            ->orderBy('i.id', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function findByKind(string $kind)
+    {
+        return $this->createQueryBuilder('b')
+            ->innerJoin('b.kinds', 'k')
+            ->andWhere('k.lastname LIKE :kind')
+            ->setParameter('kind', "%{$kind}%")
+            ->orderBy('b.lastname', 'ASC')
+            ->getQuery()
+            ->getResult()
+        ;
     }
 
     // /**
