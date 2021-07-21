fd75a0c65463b161e8e60acc0bab72723fc03083
Maryline Frémineur <maryline.fremineur@gmail.com>
2021-07-21 16:14:02 +0200

    Ajout de la liste des livres dans la page d'accueil

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -6,15 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Repository\BookRepository;
+
 class AppController extends AbstractController
 {
     /**
      * @Route("/", name="app")
      */
-    public function index(): Response
+    public function index(BookRepository $bookRepository): Response
     {
         return $this->render('app/index.html.twig', [
-            'controller_name' => 'AppController',
+            'books' => $bookRepository->findAll(),
         ]);
     }
 }
